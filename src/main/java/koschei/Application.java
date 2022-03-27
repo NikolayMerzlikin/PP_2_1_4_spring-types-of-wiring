c778e0a0dc0c11e0949f2d532126c4df302f61dd
NikolayMerzlikin <nikolaymerzlikin@gmail.com>
2022-03-27 17:33:52 +0500

    Spring 2.3

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -4,6 +4,8 @@ import koschei.config.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.sql.DataSource;
+
 public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
